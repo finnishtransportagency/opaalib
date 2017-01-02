83870bc2ab5da460fffc720509f48a1cc4251b19
Iiro Surakka <iiro.surakka@nodeon.com>
2017-01-02 12:38:56 +0200

    Add notification types

---
+++ SoneraOMA.Messaging/DeliveryInfoNotification.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoneraOMA.Messaging
+{
+    public class DeliveryInfoNotificationContainer
+    {
+        [JsonProperty(PropertyName = "deliveryInfoNotification", Required = Required.Always)]
+        public DeliveryInfoNotification DeliveryInfoNotification { get; set; }
+    }
+
+    public class DeliveryInfoNotification
+    {
+        [JsonProperty(PropertyName = "deliveryInfo", Required = Required.Always)]
+        public DeliveryInfo[] DeliveryInfo { get; set; }
+
+        [JsonProperty(PropertyName = "callbackData")]
+        public string CallbackData { get; set; }
+    }
+}
